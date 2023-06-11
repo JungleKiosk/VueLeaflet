ab25a3ab2ec1d697549f1684c474e549e6f4820d
Fra <135766845+JungleKiosk@users.noreply.github.com>
2023-06-11 18:34:02 +0200

    data: add dataframe

---
+++ src/data/markers.js
@@ -0,0 +1,67 @@
+export const marker_points = [
+    {
+        id: 0,
+        city: "",
+        lat: "",
+        long: "",
+        img_popup: "",
+        img_marker_card:
+            [
+                { img_card: "" },
+                { img_card: "" },
+                { img_card: "" }
+            ]
+    },
+    {
+        id: 1,
+        city: "",
+        lat: "",
+        long: "",
+        img_popup: "",
+        img_marker_card:
+            [
+                { img_card: "" },
+                { img_card: "" },
+                { img_card: "" }
+            ]
+    },
+    {
+        id: 2,
+        city: "",
+        lat: "",
+        long: "",
+        img_popup: "",
+        img_marker_card:
+            [
+                { img_card: "" },
+                { img_card: "" },
+                { img_card: "" }
+            ]
+    },
+    {
+        id: 3,
+        city: "",
+        lat: "",
+        long: "",
+        img_popup: "",
+        img_marker_card:
+            [
+                { img_card: "" },
+                { img_card: "" },
+                { img_card: "" }
+            ]
+    },
+    {
+        id: 4,
+        city: "",
+        lat: "",
+        long: "",
+        img_popup: "",
+        img_marker_card:
+            [
+                { img_card: "" },
+                { img_card: "" },
+                { img_card: "" }
+            ]
+    },
+];
